5bf62a15d726f166c9e692325a1fe0f4e939ddcc
Damashdam <danielburlakov@gmail.com>
2024-03-21 16:17:12 +0200

    commit message

---
+++ index.py
@@ -0,0 +1,71 @@
+#####excresise 1######
+# input = input("type a number")
+# input =int(input)
+# input = (input%2)
+
+# if input == 0:
+#     print("its a rounded number")
+    
+# else:
+#     print("its not a rounded number")
+
+#####excresise 2######
+
+# input2 = input("write your number")
+# input_int = int(input2)
+
+# if input_int > 0 :
+#     print("your number is positive")
+# elif input_int < 0 :
+#     print("your number is negative")
+# else input_int == 0 :
+#     print("your number is the same")
+
+#####excresise 3######
+
+# input2 = input("write your number")
+# input_int = int(input2)
+
+# if input_int % 5 == 0 :
+#     print("we can divide your number by 5")
+# # elif input_int < 0 :
+# #     print("your number is positive")
+# else :
+#         print("we cant divide your number by 5")
+
+#####excresise 4######
+# input2 = input("write your number")
+# input_int = int(input2)
+
+# if 0<input_int<10 :
+#     print("small")
+# elif 10<input_int<=100 :
+#     print("medium")
+# elif 1000<input_int<=1000 :
+#     print("large")
+# else :
+#     print("biggerrr")
+
+#####excresise 5######
+# input = input("write a charcter")
+
+# check_input = input.islower()
+
+# if check_input == True:
+#     print("its LowerCase")
+# else:
+#     print ("its a UpperCase")
+
+#####excresise 6######
+# input1 = input("write a number")
+# input2 = input("write a number")
+
+# if input1>input2 :
+#     print("number 1 bigger than number 2")
+    
+# if input1<input2 :
+#     print("number 2 bigger than number 1")
+# else:
+#     print("the numbers are the same")
+
+#####excresise 7######
