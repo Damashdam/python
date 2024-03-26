de1a353d51f6381d94a20f2faf88a855447543d0
Damashdam <121407251+Damashdam@users.noreply.github.com>
2024-03-26 13:23:03 +0200

    Add files via upload

--- index.py
+++ index.py
@@ -1,71 +1,11 @@
-#####excresise 1######
-# input = input("type a number")
-# input =int(input)
-# input = (input%2)
+list1 = {"hats": 5, "doors":3,"computers":2}
 
-# if input == 0:
-#     print("its a rounded number")
-    
-# else:
-#     print("its not a rounded number")
+print("would you want to change something in the list")
 
-#####excresise 2######
+input_controller = input("type add to add a product")
+if input_controller.lower() == "add" :
+    user_product = input("type the product name")  
+    amount = int(input("type the product amount"))
+list1[user_product] = amount
 
-# input2 = input("write your number")
-# input_int = int(input2)
-
-# if input_int > 0 :
-#     print("your number is positive")
-# elif input_int < 0 :
-#     print("your number is negative")
-# else input_int == 0 :
-#     print("your number is the same")
-
-#####excresise 3######
-
-# input2 = input("write your number")
-# input_int = int(input2)
-
-# if input_int % 5 == 0 :
-#     print("we can divide your number by 5")
-# # elif input_int < 0 :
-# #     print("your number is positive")
-# else :
-#         print("we cant divide your number by 5")
-
-#####excresise 4######
-# input2 = input("write your number")
-# input_int = int(input2)
-
-# if 0<input_int<10 :
-#     print("small")
-# elif 10<input_int<=100 :
-#     print("medium")
-# elif 1000<input_int<=1000 :
-#     print("large")
-# else :
-#     print("biggerrr")
-
-#####excresise 5######
-# input = input("write a charcter")
-
-# check_input = input.islower()
-
-# if check_input == True:
-#     print("its LowerCase")
-# else:
-#     print ("its a UpperCase")
-
-#####excresise 6######
-# input1 = input("write a number")
-# input2 = input("write a number")
-
-# if input1>input2 :
-#     print("number 1 bigger than number 2")
-    
-# if input1<input2 :
-#     print("number 2 bigger than number 1")
-# else:
-#     print("the numbers are the same")
-
-#####excresise 7######
+print (list1)
