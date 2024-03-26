ab3671b5e4014b8d04e50b4dd697e970e8111cd3
Damashdam <121407251+Damashdam@users.noreply.github.com>
2024-03-26 14:20:01 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,26 @@
-list1 = {"hats": 5, "doors":3,"computers":2}
+list1 = {"hats": 5, "doors": 3, "computers": 2}
+print(list1)
 
-print("would you want to change something in the list")
+print("Would you like to change something in the list?")
 
-input_controller = input("type add to add a product")
-if input_controller.lower() == "add" :
-    user_product = input("type the product name")  
-    amount = int(input("type the product amount"))
-list1[user_product] = amount
 
-print (list1)
+
+input_controller = input("Type 'add' to add a product or 'remove' to remove a product: ")
+
+if input_controller.lower() == "add":
+    user_product = input("Type the product name: ")
+    amount = int(input("Type the product amount: "))
+    list1[user_product] = amount
+    print("Product added successfully!")
+
+elif input_controller.lower() == "remove":
+    print(list1)
+    what_to_remove = input("What item do you want to remove? ")
+    if what_to_remove in list1:
+        del list1[what_to_remove]
+        print(f"The item '{what_to_remove}' has been removed.")
+    else:
+        print("Item not found in the list.")
+
+print(f"youre updated list{list1}")
+    
